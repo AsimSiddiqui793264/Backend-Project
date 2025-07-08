ace608d1e4855019f53cae1fd3b3efaf210c2516
Asim Siddiqui <asimsiddiqui939@gmail.com>
2025-07-08 07:11:25 -0700

    utils code discussions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+const PORT = process.env.PORT || 8000;
 
 dotenv.config({
     path : "./env"
 });
 
-connectDB();
+connectDB()
+.then(() =>{
+    app.listen(PORT , () =>{
+        console.log(`Server is runing on http://localhost:${PORT}`);
+    });
+
+    app.on("error" , (errro) =>{
+        console.log("ERROR : " , errro);
+        throw errro;
+    })
+})
+.catch((err) =>{
+    console.log("Monogo DB connection error is : " , err);
+})
